7d83e157f50878ae0176b91bb1f363fde3644f6c
John Hallat <jhallat@gmail.com>
2019-11-01 17:24:24 -0400

    added merge sort

--- demo.jl
+++ demo.jl
@@ -16,7 +16,7 @@ function simpleHash(value)
     abs(hash)
 end
 
-function createTree(values)
+function createTree(values::Array{Int64})
     if length(values) > 2
         midpoint = div(length(values), 2)
         Node(values[midpoint], createTree(values[1:midpoint - 1]), createTree(values[midpoint + 1:end]))
@@ -33,7 +33,45 @@ function createTree(values)
     end  
 end
 
+function mergeSort(values::Array{Int64})
+    count = length(values)
+    if count == 1
+        values
+    else  
+        midpoint = div(count, 2)
+        values1 = mergeSort(values[1:midpoint])
+        values2 = mergeSort(values[midpoint + 1:end])
+        sorted = Array{Int64}(undef, count)
+        index1 = 1
+        index2 = 1
+        indexSort = 1
+        while index1 <= length(values1) && index2 <= length(values2)
+            if values1[index1] < values2[index2]
+                sorted[indexSort] = values1[index1]
+                index1 += 1
+            else
+                sorted[indexSort] = values2[index2]
+                index2 += 1
+            end
+            indexSort += 1
+        end
+        while length(values1) >= index1
+            sorted[indexSort] = values1[index1]
+            indexSort += 1
+            index1 += 1
+        end  
+        while length(values2) >= index2
+            sorted[indexSort] = values2[index2]
+            indexSort += 1
+            index2 += 1
+        end  
+        sorted 
+    end   
+end  
+
 #println(simpleHash("This is a test"))
 #println(simpleHash("This is not a test"))
 #println(simpleHash("This could be a test"))
-println(createTree([10,20,30,40,50,60,70,80,90,100]))
+values = [100,50,60,20,10,30,90,70,80,40]
+values = mergeSort(values)
+println(createTree(values))
